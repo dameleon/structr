7b0beccb66af065391417b9103fb963ab853889c
dameleon <dameleon@gmail.com>
2016-04-05 16:30:44 +0900

    defined json schema types

---
+++ types.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	SchemaTypeNull = "null"
+	SchemaTypeBoolean = "boolean"
+	SchemaTypeString = "string"
+	SchemaTypeInteger = "integer"
+	SchemaTypeNumber = "number"
+	SchemaTypeObject = "object"
+	SchemaTypeArray = "array"
+)
