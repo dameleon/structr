56f231f8aef2d6c0197f702800ef2b1763acc0f6
dameleon <dameleon@gmail.com>
2016-04-06 12:27:15 +0900

    rename types.go to json_schema_types.go

---
+++ json_schema_types.go
@@ -0,0 +1,33 @@
+package main
+
+type SchemaType string
+
+const (
+	SchemaTypeNull SchemaType = "null"
+	SchemaTypeBoolean = "boolean"
+	SchemaTypeString = "string"
+	SchemaTypeInteger = "integer"
+	SchemaTypeNumber = "number"
+	SchemaTypeObject = "object"
+	SchemaTypeArray = "array"
+)
+
+var SchemaTypes = [...]SchemaType{
+	SchemaTypeNull,
+	SchemaTypeBoolean,
+	SchemaTypeString,
+	SchemaTypeInteger,
+	SchemaTypeNumber,
+	SchemaTypeObject,
+	SchemaTypeArray,
+}
+
+func SchemaTypeFromString(str string) (SchemaType) {
+	for _, v := range SchemaTypes {
+		if str == string(v) {
+			return v
+		}
+	}
+	println(strj
+	panic(str)
+}
