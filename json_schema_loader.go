965f360ff62d4db09cbd3831f57003f9fdb149a9
dameleon <dameleon@gmail.com>
2016-04-06 17:56:32 +0900

    JsonSchemaLoader with singleton

--- json_schema_loader.go
+++ json_schema_loader.go
@@ -7,43 +7,54 @@ import (
 	"encoding/json"
 )
 
-type JsonSchemaLoader interface {
-	Load() ([]byte, error)
+type jsonSchemaLoader struct {
+	pool map[string]JsonSchema
 }
 
-func NewJsonSchemaLoader(ref gojsonreference.JsonReference) (JsonSchemaLoader, error) {
-	// localfile...?
-	if ref.HasFileScheme || ref.GetUrl().Scheme == "" {
-		return LocalJsonSchemaLoader{ ref }
+func (l* jsonSchemaLoader) Load(ref gojsonreference.JsonReference) (JsonSchema) {
+	path := getUniquePath(ref)
+	if _, ok := l.pool[path]; !ok {
+		var schema JsonSchema
+		if ref.HasFullFilePath || ref.HasFileScheme {
+			schema = l.loadFromFile(ref)
+		} else {
+			schema = l.loadFromRemote(ref)
+		}
+		l.pool[path] = schema
 	}
-	return RemoteJsonSchemaLoader{ ref }
+	if ref.GetUrl().Fragment != "" {
+		return l.pool[path].GetRefSchema(ref.GetPointer())
+	}
+	return l.pool[path]
 }
 
-type RemoteJsonSchemaLoader struct {
-	ref gojsonreference.JsonReference
+func (l jsonSchemaLoader) loadFromRemote(ref gojsonreference.JsonReference) (JsonSchema) {
+	// TODO
+	return JsonSchema{}
 }
 
-func (l *RemoteJsonSchemaLoader) Load() ([]byte, error) {
-	// TBD
-	return []byte{}, nil
-}
-
-type LocalJsonSchemaLoader struct {
-	ref gojsonreference.JsonReference
-}
-
-func (l *LocalJsonSchemaLoader) loadJson() ([]byte) {
-	file, err := ioutil.ReadFile(l.ref.GetUrl().Path)
+func (l jsonSchemaLoader) loadFromFile(ref gojsonreference.JsonReference) (JsonSchema) {
+	j, err := ioutil.ReadFile(ref.GetUrl().Path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return file
-}
-
-func (l *LocalJsonSchemaLoader) Load() (JsonSchema) {
 	var s JsonSchema
-	if e := json.Unmarshal(l.loadJson(), s); e != nil {
+	if e := json.Unmarshal(j, &s); e != nil {
 		log.Fatal(e)
 	}
 	return s
+}
+
+var instance *jsonSchemaLoader
+
+func GetJsonSchemaLoader() *jsonSchemaLoader {
+	if instance == nil {
+		instance = &jsonSchemaLoader{ make(map[string]JsonSchema) }
+	}
+	return instance
+}
+
+func getUniquePath(ref gojsonreference.JsonReference) (string) {
+	url := ref.GetUrl()
+	return url.RequestURI()
 }
